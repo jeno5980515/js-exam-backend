d68f257e45a74259842f26c5a2ae3fd797ee889f
jeno5980515 <jeno5980515@yahoo.com.tw>
2018-10-15 15:11:23 +0800

    modfiy fileName by socketId

--- server/index.js
+++ server/index.js
@@ -5,8 +5,9 @@ const io = socket();
 io.on('connection', (client) => {
 	client.on('code', (data) => {
 		try {
-			fs.appendFileSync('message.txt', data);
-			fs.appendFileSync('message.txt', '\n$$$$$$$$$$$$$$$$$$$$$$$\n');
+			const fileName = `${client.id}-${data.question}.txt`;
+			fs.appendFileSync(fileName, data.code);
+			fs.appendFileSync(fileName, '\n$$$$$$$$$$$$$$$$$$$$$$$\n');
 		} catch (err) {
 			console.log(err);
 			/* Handle the error */
